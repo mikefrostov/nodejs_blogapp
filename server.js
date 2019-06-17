887a8dad5e7721d619c60ab4d34ee90be8bc911f
mikefrostov <m.e.morozov1@gmail.com>
2019-06-17 22:37:10 +0300

    Prometheus endpoint

--- server.js
+++ server.js
@@ -1,16 +1,74 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
+const Prometheus = require('prom-client')
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const app = express()
+const port = process.env.PORT || 8080
+const metricsInterval = Prometheus.collectDefaultMetrics()
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]  // buckets for response time from 0.1ms to 500ms
+})
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
+// Runs before each requests
+app.use((req, res, next) => {
+  res.locals.startEpoch = Date.now()
+  next()
+})
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/', (req, res, next) => {
+  setTimeout(() => {
+    res.json({ message: 'Hello World!' })
+    next()
+  }, Math.round(Math.random() * 200))
+})
+
+app.get('/bad', (req, res, next) => {
+  next(new Error('My Error'))
+})
+
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(Prometheus.register.metrics())
+})
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.statusCode = 500
+  // Do not expose your error in production
+  res.json({ error: err.message })
+  next()
+})
+
+// Runs after each requests
+app.use((req, res, next) => {
+  const responseTimeInMs = Date.now() - res.locals.startEpoch
+
+  httpRequestDurationMicroseconds
+    .labels(req.method, req.route.path, res.statusCode)
+    .observe(responseTimeInMs)
+
+  next()
+})
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+})
+
+// Graceful shutdown
+process.on('SIGTERM', () => {
+  clearInterval(metricsInterval)
+
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    process.exit(0)
+  })
+})
