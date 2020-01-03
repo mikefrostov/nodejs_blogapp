cf45d232f42697f96a22ead44e6b58a78e79e9a1
mikefrostov <m.e.morozov1@gmail.com>
2020-01-03 16:56:27 +0300

    fix mongo ct script

--- mongodbct/dockerrun.sh
+++ mongodbct/dockerrun.sh
@@ -1,2 +1,2 @@
 docker build -t mymongo .
-docker run --name mongo-dev -d -v /opt/mongodb:/data/db -p 27017:27017 mymongo
+docker run --name mongo-dev -d -v $PWD/data/db:/data/db -p 27017:27017 mymongo
